112b74896586bd7193de201fff1a28ab1c513044
Stefanos Tsaklidis <stefanos@tsaklidis.gr>
2024-12-26 22:30:25 +0200

    Add get_working_days() and get_company_holidays() methods

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import base64
+from typing import Union, List
+
 import requests
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
+
 
 from settings.vars import debug
 from employees.models import EmployeeActions
@@ -108,7 +111,7 @@ class BambooTimeOff:
 
         return available_employees
 
-    def get_available_employees_v2(self, start, end, sector=None):
+    def get_available_employees_v2(self, start:str, end:str, sector=None) -> list:
         """
         Get available employees with the use of '/time_off/whos_out/' endpoint.
         This endpoint can show employees without the use of an API key that
@@ -140,8 +143,51 @@ class BambooTimeOff:
 
         return employees_objs
 
+    def get_company_holidays(self, start:str, end:str) -> list:
+        """
+        This function retrieves company holidays within a specified date range.
+        The logic is:
+        1.Getting a list of employees who are out of office (get_who_is_out_employees)
+        2.Filtering this list to only include items where the type is "holiday"
+        3.Extracting the start date of each holiday and converting it to a date object
+        4.Returning a list of these holiday dates
+        """
+        items = self.get_who_is_out_employees(start, end)
+        holidays_dates = []
+        for item in items:
+            if item.get("type") == "holiday":
+                tmp_date = date.fromisoformat(item.get("start"))
+                holidays_dates.append(tmp_date)
+        return holidays_dates
 
+    def get_working_days(self, start:str, end:str, return_total=False) -> Union[int, list[date]]:
+        """
+        Calculate the number of working days between two dates.
+        Working days are considered to be Monday through Friday, excluding weekends and holidays.
+        Args:
+            start (str): The start date.
+            end (str): The end date.
+        Returns:
+            list: Returns a list of datetime.date(2024, 12, 23)
+            If "return_total" flag is se to True
+                int: The number of working days between the start and end dates.
 
+        """
 
+        working_week_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+        start_date = date.fromisoformat(start)
+        end_date = date.fromisoformat(end)
+        company_holidays = self.get_company_holidays(start, end)
+
+        working_dates = []
+        current_date = start_date
+        while current_date <= end_date:
+            if current_date.strftime("%A") in working_week_days and current_date not in company_holidays:
+                working_dates.append(current_date)
+            current_date += timedelta(days=1)
+
+        if return_total:
+            return len(working_dates)
+        return working_dates
 
 
